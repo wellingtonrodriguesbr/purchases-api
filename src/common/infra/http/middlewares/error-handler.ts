b247209954863999bf57ede08ccc5bfdee91ea16
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-10-14 08:17:38 -0300

    feat: middleware error handler

---
+++ src/common/infra/http/middlewares/error-handler.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "@/common/domain/errors/app-error";
+
+export function errorHandler(
+  error: Error,
+  req: Request,
+  res: Response,
+  _next: NextFunction,
+): Response {
+  if (error instanceof AppError) {
+    return res
+      .status(error.statusCode)
+      .json({ status: "error", message: error.message });
+  }
+
+  console.error(error);
+
+  return res
+    .status(500)
+    .json({ status: "error", message: "Internal server error" });
+}
