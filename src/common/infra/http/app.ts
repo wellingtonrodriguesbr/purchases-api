161f07d8afb7c0de6ba3970ada4a05990d6c2f8e
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-10-14 08:29:01 -0300

    chore: add middleware on app.ts

--- src/common/infra/http/app.ts
+++ src/common/infra/http/app.ts
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 
 import { routes } from "./routes";
+import { errorHandler } from "./middlewares/error-handler";
 
 export const app = express();
 
@@ -9,3 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  errorHandler(error, req, res, next);
+});
