fc23971665dbddb8b1578d01570312ede047975d
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-10-21 07:21:17 -0300

    chore: config swagger

--- src/common/infra/http/app.ts
+++ src/common/infra/http/app.ts
@@ -4,10 +4,28 @@ import cors from "cors";
 import { routes } from "./routes";
 import { errorHandler } from "./middlewares/error-handler";
 
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const swaggerJSDocOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Purchases API Documentation",
+      version: "1.0.0",
+    },
+  },
+  apis: [],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerJSDocOptions);
+
 export const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(routes);
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
