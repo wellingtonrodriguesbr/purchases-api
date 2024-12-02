38d038009b6935276e89846fc99d46a182dd29d5
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-12-02 07:06:38 -0300

    feat: create product repository

---
+++ src/products/domain/repositories/products-repository.ts
@@ -0,0 +1,22 @@
+import { CommonRepository } from "@/common/domain/repositories/common-repository";
+import { ProductModel } from "@/products/domain/models/products-model";
+
+export type ProductId = {
+  id: string;
+};
+
+export type CreateProductProps = {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+  created_at: Date;
+  updated_at: Date;
+};
+
+export interface ProductsRepository
+  extends CommonRepository<ProductModel, CreateProductProps> {
+  findByName(name: string): Promise<ProductModel>;
+  findAllByIds(ids: ProductId[]): Promise<ProductModel[]>;
+  conflictingName(name: string): Promise<void>;
+}
