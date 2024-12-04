7e7b9153e9223921743936ffbc837bbc695b395d
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-12-04 08:34:27 -0300

    feat: create helper to products data builder

---
+++ src/products/infra/testing/helpers/products-data-builder.ts
@@ -0,0 +1,24 @@
+import { ProductModel } from "@/products/domain/models/products-model";
+import { faker } from "@faker-js/faker";
+import { randomUUID } from "node:crypto";
+
+export function ProductsDataBuilder(
+  props: Partial<ProductModel>,
+): ProductModel {
+  return {
+    id: props.id ?? randomUUID(),
+    name: props.name ?? faker.commerce.productName(),
+    price:
+      props.price ??
+      Number(
+        faker.commerce.price({
+          min: 100,
+          max: 2000,
+          dec: 2,
+        }),
+      ),
+    quantity: props.quantity ?? 10,
+    created_at: props.created_at ?? new Date(),
+    updated_at: props.updated_at ?? new Date(),
+  };
+}
