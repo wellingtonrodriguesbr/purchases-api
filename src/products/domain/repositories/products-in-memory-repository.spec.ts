9e172cbc964ba007238d2b3c8d581e70409b36fa
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2025-03-01 09:49:22 -0300

    test: test ConflictingName method

--- src/products/domain/repositories/products-in-memory-repository.spec.ts
+++ src/products/domain/repositories/products-in-memory-repository.spec.ts
@@ -1,3 +1,4 @@
+import { ConflictError } from "@/common/domain/errors/conflict-error";
 import { NotFoundError } from "@/common/domain/errors/not-found-error";
 import { ProductsInMemoryRepository } from "@/products/domain/repositories/products-in-memory-repository";
 import { ProductsDataBuilder } from "@/products/infra/testing/helpers/products-data-builder";
@@ -14,6 +15,10 @@ describe("ProductsInMemoryRepository Unit Tests", () => {
       await expect(sut.findByName("any_name")).rejects.toThrow(
         new NotFoundError("Product not found using name any_name"),
       );
+
+      await expect(sut.findByName("any_name")).rejects.toBeInstanceOf(
+        NotFoundError,
+      );
     });
 
     it("should return product when product is found", async () => {
@@ -24,6 +29,26 @@ describe("ProductsInMemoryRepository Unit Tests", () => {
       const result = await sut.findByName("any_name");
 
       expect(result).toStrictEqual(product);
+    });
+  });
+
+  describe("ConflictingName", () => {
+    it("should throw error when product is found", async () => {
+      const product1 = ProductsDataBuilder({ name: "any_name" });
+
+      sut.items.push(product1);
+
+      await expect(sut.conflictingName("any_name")).rejects.toThrow(
+        new ConflictError("Product with name any_name already exists"),
+      );
+      await expect(sut.conflictingName("any_name")).rejects.toBeInstanceOf(
+        ConflictError,
+      );
+    });
+
+    it("should not find a product by name", async () => {
+      expect.assertions(0);
+      await sut.conflictingName("any_name");
     });
   });
 });
